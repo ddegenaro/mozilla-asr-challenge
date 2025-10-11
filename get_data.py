31a8d5e36fb0dd420ec5966dde898deb3dc930c0
Dan <daniel.degenaro@gmail.com>
2025-10-11 01:20:16 -0400

    initial commit

---
+++ get_data.py
@@ -0,0 +1,116 @@
+import os
+from typing import Union, Iterable
+
+import pandas as pd
+from torch.utils.data import Dataset
+
+ROOT = os.path.join(
+    '/home', 'drd92', 'mozilla-asr-challenge', 'mcv-sps-st-09-2025'
+)
+
+LANGUAGES = {
+    'aln',
+    'bew',
+    'bxk',
+    'cgg',
+    'el-CY',
+    'hch',
+    'kcn',
+    'koo',
+    'led',
+    'lke',
+    'lth',
+    'meh',
+    'mmc',
+    'pne',
+    'ruc',
+    'rwm',
+    'sco',
+    'tob',
+    'top',
+    'ttj',
+    'ukv'
+}
+
+class SpeechDataset(Dataset):
+
+    def __init__(self, split: str, langs: Iterable[str], df: pd.DataFrame):
+        super().__init__()
+        self.split = split
+        self.langs = langs
+        self.df = df
+
+    def __len__(self):
+        """Returns the length of the dataset in audio-text pairs.
+
+        Returns:
+            `int`: The length.
+        """
+        return len(self.df)
+    
+    def __getitem__(self, index):
+        """Indexing operations.
+
+        Args:
+            index: _The row(s) to select._
+
+        Returns:
+            _dict_: _The keys are `meta`, `audios` (absolute paths to audio files) and `transcriptions`._
+        """
+
+        if type(index) == int:
+            index = [index]
+        
+        rows = self.df.loc[index]
+
+        audio_files = rows['audio_file']
+
+        langs = [
+            x.split('-')[2] for x in audio_files
+        ]
+
+        audios = [
+            os.path.join(ROOT, f'sps-corpus-1.0-2025-09-05-{lang}', audio_file)
+            for audio_file, lang in zip(audio_files, langs)
+        ]
+
+        transcriptions = rows['transcription'].tolist()
+
+        return {
+            'meta': rows,
+            'audios': audios,
+            'transcriptions': transcriptions
+        }
+
+def get_data(split: str, langs: Union[str, Iterable[str]] = None) -> SpeechDataset:
+    """Indexing operations.
+
+    Args:
+        split (_str_): _The split to return (either `train` or `dev`)._
+        langs (_Union[str, Iterable[str]]_): _The languages to include. Defaults to `None`, which inludes all languages._
+
+    Returns:
+        SpeechDataset: _A dataset that supports PyTorch `Dataset` functionality._
+    """
+
+    if type(langs) == str:
+        langs = [langs]
+    elif langs is None:
+        langs = LANGUAGES
+
+    dfs = []
+
+    for lang in langs:
+        assert lang in LANGUAGES, f'No such lang: {lang}'
+        lang_dir = os.path.join(ROOT, f'sps-corpus-1.0-2025-09-05-{lang}')
+
+        lang_df = pd.read_csv(
+            os.path.join(lang_dir, f'ss-corpus-{lang}.tsv'),
+            sep='\t'
+        )
+
+        dfs.append(lang_df[lang_df['split'] == split])
+
+    df = pd.concat(dfs, ignore_index=True)
+
+    return SpeechDataset(split, langs, df)
