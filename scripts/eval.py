b9bff81612ce215775ae1764ef950261298d9260
Emma Rafkin <emmarafkin@gmail.com>
2025-11-13 12:37:09 -0500

    reran whisper tiny results

--- scripts/eval.py
+++ scripts/eval.py
@@ -7,12 +7,12 @@ import librosa
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from utils.whisper_data_collator import WhisperDataCollator
-from utils.lang_maps import LANGUAGES
+from utils.lang_maps import LANGUAGES, ALL_TARGETS
 import torch
 from jiwer import wer
 import numpy as np
 from utils.clean_transcript import clean
-from peft import LoraConfig, get_peft_model
+from peft import LoraConfig, get_peft_model, PeftModel
 
 
 with open("config.json", "r") as f:
@@ -35,7 +35,8 @@ def evaluate(model, data, processor):
             # Generate output token IDs
             predicted_ids = model.generate(
                 inputs,
-                forced_decoder_ids=forced_decoder_ids
+                forced_decoder_ids=forced_decoder_ids,
+                max_new_tokens=200
             )
             transcriptions = processor.batch_decode(predicted_ids, skip_special_tokens=True)
             labs = [np.where(l != -100, l, processor.tokenizer.pad_token_id) for l in batch["labels"]] 
@@ -53,17 +54,10 @@ if __name__ == "__main__":
     model_dir = f"output_{config['whisper_model'].split('/')[1]}"
     split = 'dev'
     overall_rows = []
-    for lang in LANGUAGES:
+    for lang in ALL_TARGETS:
         data = get_data(split=split, langs=None if lang == "all" else [lang])
-        if config['lora']: 
-            lora_config = LoraConfig(
-                r=config["lora_rank"],
-                lora_alpha=32,
-                lora_dropout=0.05,
-                bias="none",
-                target_modules=["q_proj", "k_proj", "v_proj", "out_proj"], 
-                )       
-             # quantize    
+        if config['lora']:  
+             # quantize
             bnb_config = BitsAndBytesConfig(
                 load_in_4bit=True,
                 bnb_4bit_use_double_quant=True,
@@ -71,10 +65,10 @@ if __name__ == "__main__":
                 bnb_4bit_compute_dtype=torch.float16
             )
             model = WhisperForConditionalGeneration.from_pretrained(config["whisper_model"], quantization_config=bnb_config)
-            model = get_peft_model(model, lora_config)
+            model = PeftModel.from_pretrained(model, f"{model_dir}/{lang}/final")
             model.print_trainable_parameters()
         else:
-            model = WhisperForConditionalGeneration.from_pretrained(model_str)
+            model = WhisperForConditionalGeneration.from_pretrained(f"{model_dir}/{lang}/final")
         proxy_lang = config["proxy_langs"][lang]
         processor = WhisperProcessor.from_pretrained(config["whisper_model"], language=proxy_lang, task="transcribe")
         predictions, labels, wers = evaluate(model, data, processor)
