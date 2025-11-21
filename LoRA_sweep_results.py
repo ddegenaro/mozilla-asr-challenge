3ad991c3aa212e8d5c8c18f4533c298becb1c9ed
Emma Rafkin <emmarafkin@gmail.com>
2025-11-21 14:46:48 -0500

    Lora hyperparamters (#14)
    * new script for lora sweep
    
    * slurm for lora
    
    * lora sweep results are in
    
    * moved sweep results to the results folder

---
+++ LoRA_sweep_results.py
@@ -0,0 +1,326 @@
+[
+    {
+        "lora_rank": 8,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj"
+        ],
+        "results": {
+            "eval_loss": 1.1554893255233765,
+            "eval_wer": 64.49248537795216,
+            "eval_runtime": 245.9459,
+            "eval_samples_per_second": 0.451,
+            "eval_steps_per_second": 0.028,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 8,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj"
+        ],
+        "results": {
+            "eval_loss": 1.1375665664672852,
+            "eval_wer": 65.03294587991412,
+            "eval_runtime": 282.5427,
+            "eval_samples_per_second": 0.393,
+            "eval_steps_per_second": 0.025,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 8,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj",
+            "fc_1",
+            "fc_2"
+        ],
+        "results": {
+            "eval_loss": 1.1375848054885864,
+            "eval_wer": 64.84045309839343,
+            "eval_runtime": 279.6595,
+            "eval_samples_per_second": 0.397,
+            "eval_steps_per_second": 0.025,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 8,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj"
+        ],
+        "results": {
+            "eval_loss": 1.472395658493042,
+            "eval_wer": 54.46065003331606,
+            "eval_runtime": 247.5881,
+            "eval_samples_per_second": 0.448,
+            "eval_steps_per_second": 0.028,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 8,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj"
+        ],
+        "results": {
+            "eval_loss": 1.1145485639572144,
+            "eval_wer": 60.953579625379426,
+            "eval_runtime": 279.8575,
+            "eval_samples_per_second": 0.397,
+            "eval_steps_per_second": 0.025,
+            "epoch": 6.0
+        }
+    },
+    {
+        "lora_rank": 8,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj",
+            "fc_1",
+            "fc_2"
+        ],
+        "results": {
+            "eval_loss": 1.3713470697402954,
+            "eval_wer": 60.531576219737914,
+            "eval_runtime": 279.095,
+            "eval_samples_per_second": 0.398,
+            "eval_steps_per_second": 0.025,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 32,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj"
+        ],
+        "results": {
+            "eval_loss": 1.1269164085388184,
+            "eval_wer": 60.73147256977863,
+            "eval_runtime": 247.4895,
+            "eval_samples_per_second": 0.449,
+            "eval_steps_per_second": 0.028,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 32,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj"
+        ],
+        "results": {
+            "eval_loss": 1.1363465785980225,
+            "eval_wer": 74.85748130598948,
+            "eval_runtime": 278.3115,
+            "eval_samples_per_second": 0.399,
+            "eval_steps_per_second": 0.025,
+            "epoch": 6.0
+        }
+    },
+    {
+        "lora_rank": 32,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj",
+            "fc_1",
+            "fc_2"
+        ],
+        "results": {
+            "eval_loss": 1.136169672012329,
+            "eval_wer": 75.10920263567039,
+            "eval_runtime": 282.4526,
+            "eval_samples_per_second": 0.393,
+            "eval_steps_per_second": 0.025,
+            "epoch": 6.0
+        }
+    },
+    {
+        "lora_rank": 32,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj"
+        ],
+        "results": {
+            "eval_loss": 1.2926222085952759,
+            "eval_wer": 65.68445990967646,
+            "eval_runtime": 248.2721,
+            "eval_samples_per_second": 0.447,
+            "eval_steps_per_second": 0.028,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 32,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj"
+        ],
+        "results": {
+            "eval_loss": 1.0904053449630737,
+            "eval_wer": 59.79862293625528,
+            "eval_runtime": 283.3994,
+            "eval_samples_per_second": 0.392,
+            "eval_steps_per_second": 0.025,
+            "epoch": 5.0
+        }
+    },
+    {
+        "lora_rank": 32,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj",
+            "fc_1",
+            "fc_2"
+        ],
+        "results": {
+            "eval_loss": 1.0896732807159424,
+            "eval_wer": 59.59872658621455,
+            "eval_runtime": 282.2533,
+            "eval_samples_per_second": 0.393,
+            "eval_steps_per_second": 0.025,
+            "epoch": 5.0
+        }
+    },
+    {
+        "lora_rank": 64,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj"
+        ],
+        "results": {
+            "eval_loss": 1.1151111125946045,
+            "eval_wer": 69.65277263641076,
+            "eval_runtime": 248.8284,
+            "eval_samples_per_second": 0.446,
+            "eval_steps_per_second": 0.028,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 64,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj"
+        ],
+        "results": {
+            "eval_loss": 1.107047438621521,
+            "eval_wer": 73.11764270378322,
+            "eval_runtime": 281.6405,
+            "eval_samples_per_second": 0.394,
+            "eval_steps_per_second": 0.025,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 64,
+        "unfreeze_token_embeddings": true,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj",
+            "fc_1",
+            "fc_2"
+        ],
+        "results": {
+            "eval_loss": 1.1072453260421753,
+            "eval_wer": 73.52483897238469,
+            "eval_runtime": 285.3568,
+            "eval_samples_per_second": 0.389,
+            "eval_steps_per_second": 0.025,
+            "epoch": 4.0
+        }
+    },
+    {
+        "lora_rank": 64,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj"
+        ],
+        "results": {
+            "eval_loss": 1.1484283208847046,
+            "eval_wer": 77.07114829347745,
+            "eval_runtime": 247.8904,
+            "eval_samples_per_second": 0.448,
+            "eval_steps_per_second": 0.028,
+            "epoch": 5.0
+        }
+    },
+    {
+        "lora_rank": 64,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj"
+        ],
+        "results": {
+            "eval_loss": 1.0756759643554688,
+            "eval_wer": 69.03827644924854,
+            "eval_runtime": 285.0026,
+            "eval_samples_per_second": 0.389,
+            "eval_steps_per_second": 0.025,
+            "epoch": 5.0
+        }
+    },
+    {
+        "lora_rank": 64,
+        "unfreeze_token_embeddings": false,
+        "target_modules": [
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "out_proj",
+            "fc_1",
+            "fc_2"
+        ],
+        "results": {
+            "eval_loss": 1.0756866931915283,
+            "eval_wer": 69.0530835862886,
+            "eval_runtime": 284.8406,
+            "eval_samples_per_second": 0.39,
+            "eval_steps_per_second": 0.025,
+            "epoch": 5.0
+        }
+    }
+]
---
+++ scripts/slurm/lora_hyp_sweep.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name="lora_sweep"
+#SBATCH --nodes=1
+#SBATCH --partition=base
+#SBATCH --output=logs/"%x.o%j"               
+#SBATCH --error=logs/"%x.e%j" 
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --mem=15G
+#SBATCH --time=60:00:00
+
+module load cuda/12.5
+
+module load gcc/11.4.0
+ 
+python3.11 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3.11 --version
+export WANDB_API_KEY='7eb373ebc11ddf9cefc6b415e3c9b40a4c9768b3'
+export WANDB_PROJECT="mozilla_asr"
+python3.11 ./scripts/search_lora_vals.py
